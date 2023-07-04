5c4eca8f73ed9490905697b6d731c68f6837a4ac
meryeliz <lizzybtk@outlook.com>
2023-07-03 21:12:21 -0300

    agregando productos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,9 +19,12 @@ const NavBar = () => {
           <Navbar.Brand as={Link} to={"/"} className='estiloNavText' >TiendaTech</Navbar.Brand>
 
           <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/"} className='estiloNavText' >Productos</Nav.Link>
+            <Nav.Link as={Link} to={"/categoria/1"} className='estiloNavText' >Alcatel</Nav.Link>
             <Nav.Link as={Link} to={"/categoria/2"} className='estiloNavText' >Iphone</Nav.Link>
             <Nav.Link as={Link} to={"/categoria/3"} className='estiloNavText' >Samsung</Nav.Link>
+            <Nav.Link as={Link} to={"/categoria/4"} className='estiloNavText' >Motorola</Nav.Link>
+            <Nav.Link as={Link} to={"/categoria/5"} className='estiloNavText' >Quantum</Nav.Link>
+            <Nav.Link as={Link} to={"/categoria/6"} className='estiloNavText' >TCL</Nav.Link>
             <Nav.Link as={Link} to={"/contacto"} className='estiloNavText' >Contacto</Nav.Link>
             <Nav.Link as={Link} to={"/iniciarSesion"} className='estiloNavText' >Iniciar Sesión</Nav.Link>
           </Nav>
