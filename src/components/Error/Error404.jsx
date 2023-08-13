1b0d63eb3bf8f5d0d805f6e67ecd49c1d4cda6a7
meryeliz <lizzybtk@outlook.com>
2023-08-13 19:00:46 -0300

    componente error 404

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -1,11 +1,21 @@
-
+import { Link } from "react-router-dom"
+import "./Error404.css"
 
 const Error404 = () => {
   return (
-    <div>
-      <h1>Error404</h1>
-      
-    </div>
+    <section className="sectionError">
+      <div className="boxError">
+        <div className="boxErrorTexto">
+          <h2>Error 404</h2>
+          <h3>Lo siento...</h3>
+          <h3>No encontramos la página que estás buscando.</h3>
+        </div>
+        <div className="boxVolverMenu">
+          <img className="imgError" src="/imagenes/error.png" alt="Imagen de celular en reparación." />
+          <Link to={"/"} className="btnVolverInicio" >Volver al inicio</Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
